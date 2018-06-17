c26449b9c34d7acd5c0e658434e948fbe016fd4c
zhoumingque <mail@zhoumq.cn>
2018-06-17 17:20:59 +0800

    [test]: test load

--- tests/load.js
+++ tests/load.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const should = require('should');
 const utils = require('../app/utils/tool');
 const Loads = require('../app/load');
